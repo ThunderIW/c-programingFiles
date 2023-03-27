a8fbe1d37340a4b3ae934627d0b446e06cf83a8e
ThunderIW <43197270+ThunderIW@users.noreply.github.com>
2023-03-27 02:20:42 +0000

    added the 2DArray.c file

---
+++ 2Darray.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main() {
+  // 2D array = An array, where each element is an entire array
+  //  useful in situation involving the need for matrix, grid , or a table
+  // of data
+
+  return 0;
+}
